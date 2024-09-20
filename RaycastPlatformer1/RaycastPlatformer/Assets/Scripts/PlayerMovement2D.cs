6b9f88f14171e4a5b45f0d8ba95e7da9d34df93a
rodrigorparedes <rodrigorparedes@gmail.com>
2024-09-19 22:03:37 -0400

    game version 2

---
+++ RaycastPlatformer1/RaycastPlatformer/Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1f, LayerMask.GetMask("Ground"));
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * 1f);
+    }
+}
